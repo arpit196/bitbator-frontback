044e7c77b72bff69b378311f8d6bbd0af2e4a9fe
Arpit Rai <arpitrai@amdocs.com>
2021-07-14 00:39:37 +0530

    added package on root

--- collab/migrations/0008_tags_tagname.py
+++ collab/migrations/0008_tags_tagname.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='tags',
             name='tagName',
-            field=models.CharField(default=builtins.any, max_length=200),
+            field=models.CharField(max_length=200),
             preserve_default=False,
         ),
     ]
