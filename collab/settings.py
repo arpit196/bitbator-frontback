41c9829e8c9b3e0bd7bb261f0c47e351e8dd718d
Arpit Rai <arpitrai@amdocs.com>
2021-07-18 20:01:28 +0530

    settings allowed host changed

--- collab/settings.py
+++ collab/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '^7ky##!@@k64q#wn=c=95_uok3%e9r=fiw0zmo&ajrj=o8e085'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bitbator-full.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
